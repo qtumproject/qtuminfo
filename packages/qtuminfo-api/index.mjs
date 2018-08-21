df7155a7638b988cc2f848fd10454eb09d3213d0
Xuan Yan <xuanyan@xuanyan.ws>
2018-08-21 18:20:50 +0800

    addresses api

--- packages/qtuminfo-api/index.mjs
+++ packages/qtuminfo-api/index.mjs
@@ -6,6 +6,7 @@ import compress from 'koa-compress'
 import bodyparser from 'koa-bodyparser'
 import Service from 'qtuminfo-node/lib/services/base'
 import RateLimiter from './components/rate-limiter'
+import AddressesController from './controllers/addresses'
 import BlocksController from './controllers/blocks'
 import TransactionsController from './controllers/transactions'
 
@@ -15,6 +16,7 @@ export default class QtuminfoAPIService extends Service {
     this.rateLimiterOptions = options.rateLimiterOptions
     this.disableRateLimiter = options.disableRateLimiter
     this._routePrefix = options.routePrefix || this.name
+    this.addresses = new AddressesController(this.node)
     this.blocks = new BlocksController(this.node)
     this.transactions = new TransactionsController(this.node)
   }
@@ -64,7 +66,67 @@ export default class QtuminfoAPIService extends Service {
       }
     })
 
+    app.use(async (ctx, next) => {
+      if (!['GET', 'POST'].includes(ctx.method)) {
+        return await next()
+      }
+      let object = {GET: ctx.query, POST: ctx.request.body}[ctx.method]
+      ctx.state.pagination = {}
+      if ('pageSize' in object) {
+        ctx.state.pageSize = Number.parseInt(object.pageSize)
+        if (!(ctx.state.pageSize > 0)) {
+          ctx.throw(400)
+        }
+      }
+      if ('page' in object) {
+        ctx.state.pageIndex = Number.parseInt(object.page)
+        if (!(ctx.state.pageIndex >= 0)) {
+          ctx.throw(400)
+        }
+      }
+      if ('reversed' in object) {
+        ctx.state.reversed = ![false, 'false', 0, '0'].includes(object.reversed)
+      }
+      await next()
+    })
+
     let router = new Router()
+
+    router.get(
+      '/address/:address',
+      this.addresses.checkAddresses.bind(this.addresses),
+      this.addresses.summary.bind(this.addresses)
+    )
+    router.get(
+      '/address/:address/balance',
+      this.addresses.checkAddresses.bind(this.addresses),
+      this.addresses.balance.bind(this.addresses)
+    )
+    router.get(
+      '/address/:address/mature-balance',
+      this.addresses.checkAddresses.bind(this.addresses),
+      this.addresses.matureBalance.bind(this.addresses)
+    )
+    router.get(
+      '/address/:address/utxo',
+      this.addresses.checkAddresses.bind(this.addresses),
+      this.addresses.utxo.bind(this.addresses)
+    )
+    router.get(
+      '/address/:address/txs',
+      this.addresses.checkAddresses.bind(this.addresses),
+      this.addresses.utxo.bind(this.addresses)
+    )
+    router.get(
+      '/address/:address/balance-history',
+      this.addresses.checkAddresses.bind(this.addresses),
+      this.addresses.balanceHistory.bind(this.addresses)
+    )
+    router.get(
+      '/address/:address/qrc20-balance-history',
+      this.addresses.checkAddresses.bind(this.addresses),
+      this.addresses.qrc20BalanceHistory.bind(this.addresses)
+    )
 
     router.get('/blocks', this.blocks.list.bind(this.blocks))
     router.get('/block/:block', this.blocks.block.bind(this.blocks))
